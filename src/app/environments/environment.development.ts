3c72f56a5280375b032a3c1930a339bce039f5bc
Rahul Rana <rahulrana723@gmail.com>
2025-04-02 16:58:46 +0530

    Redirect to loading after login

--- src/app/environments/environment.development.ts
+++ src/app/environments/environment.development.ts
@@ -1,7 +1,7 @@
 export const environment = {
     production: true,
     agentApiUrl: 'http://localhost:8001',
-    propertyApiUrl: 'http://localhost:8002',
+    propertyApiUrl: 'http://localhost:9001',
     customerUrl: '/customers',
     templateUrl: '/templates',
     roleUrl: '/roles/',
