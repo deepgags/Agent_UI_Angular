3754d7dd8860c582c186c74be973acf34b6b45dd
Aniket <tech.aniket34@gmail.com>
2025-04-02 16:21:06 +0530

    new registration formn

--- src/app/environments/environment.ts
+++ src/app/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: true,
-    agentApiUrl: 'http://localhost:8001',
+    agentApiUrl: 'http://localhost:9001',
     propertyApiUrl: 'http://localhost:8002',
     customerUrl: '/customers',
     templateUrl: '/templates',
