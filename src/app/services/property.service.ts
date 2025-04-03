e0642afbf888292fdbbffbf873d1f3bb8530b860
Rahul Rana <rahulrana723@gmail.com>
2025-04-03 14:43:33 +0530

    Remove extra code and added dummy data for properties

--- src/app/services/property.service.ts
+++ src/app/services/property.service.ts
@@ -17,48 +17,6 @@ export class PropertyService {
     this.Apiurl = environment.propertyApiUrl + environment.propertySearchUrl;
   }
 
-  // async getProperties(name:string, pageNo=1, pageSize=5): Promise<PropertyModel[]> {
-  //   this.http.get<any>(this.Apiurl + '?page=' + pageNo + '&pageSize=' + pageSize)
-  //       .subscribe(data =>
-  //           {
-  //               data.data.forEach((property: any) => {
-  //                   this.properties.push({
-  //                       _id: property._id,
-  //                       BuildingName: property.BuildingName,
-  //                       BathroomsTotalInteger: property.BathroomsTotalInteger,
-  //                       BedroomsTotal: property.BedroomsTotal,
-  //                       BrokerFaxNumber: property.BrokerFaxNumber,
-  //                       BusinessName: property.BusinessName,
-  //                       City: property.City,
-  //                       CityRegion: property.CityRegion,
-  //                       Country: property.Country,
-  //                       CountyOrParish: property.CountyOrParish,
-  //                       CrossStreet: property.CrossStreet,
-  //                       Latitude: property.Latitude,
-  //                       ListingKey:property.ListingKey,
-  //                       ListPrice:property.ListPrice,
-  //                       Longitude:property.Longitude,
-  //                       LotSizeDimensions:property.LotSizeDimensions,
-  //                       OriginalListPrice:property.OriginalListPrice,
-  //                       PrivateRemarks:property.PrivateRemarks,
-  //                       PropertySubType:property.PropertySubType,
-  //                       PropertyType:property.PropertyType,
-  //                       PropertyUse:property.PropertyUse,
-  //                       PublicRemarks:property.PublicRemarks,
-  //                       StreetName:property.StreetName,
-  //                       StreetNumber:property.StreetNumber,
-  //                       StreetSuffix:property.StreetSuffix,
-  //                       Town:property.Town,
-  //                       UnitNumber:property.UnitNumber,
-  //                       UnparsedAddress:property.UnparsedAddress,
-  //                       Media:property.Media,
-  //                   });
-  //               });
-  //           });
-
-  //    return this.properties;
-  // }
-
   searchProperties(propertyParams:any): Observable<PropertyModel[]> {
     const { page, pageSize, address, property_type, property_subtype, bedrooms, bathrooms, property_for, min_price, max_price, min_area } = propertyParams;
     return this.http.get<PropertyModel[]>(`${this.Apiurl}?page=${page}&pageSize=${pageSize},&address=${address}&property_type=${property_type}&property_subtype=${property_subtype}&bedrooms=${bedrooms}&bathrooms=${bathrooms}&property_for=${property_for}&min_price=${min_price}&max_price=${max_price}&min_area=${min_area}`)
