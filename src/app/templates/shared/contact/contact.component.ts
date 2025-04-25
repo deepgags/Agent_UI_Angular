83a90527f3ae741628e1a86f01ee2b9c3eeb527a
aniketanand <anandaniket167@gmail.com>
2025-04-25 16:07:53 +0530

    added form in contact us page

--- src/app/templates/shared/contact/contact.component.ts
+++ src/app/templates/shared/contact/contact.component.ts
@@ -1,11 +1,41 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatDialogModule } from '@angular/material/dialog';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { SearchComponent } from '../search/search.component';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { RouterModule } from '@angular/router';
+import { PhoneSearch } from '../../../Pipes/phoneSearch';
+import { UpperCase } from '../../../Pipes/upper';
 
 @Component({
   selector: 'app-contact',
-  imports: [],
+  imports: [CommonModule, NgbModule, FormsModule, ReactiveFormsModule, MatDialogModule, SearchComponent,
+    MatFormFieldModule, MatInputModule,
+    RouterModule ,PhoneSearch, UpperCase
+    ],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.scss'
 })
 export class ContactComponent {
+  userForm!: FormGroup;
+
+  constructor(     
+    private fb: FormBuilder
+ ){}
+
+ ngOnInit(): void {
+
+
+   this.userForm = this.fb.group({
+     firstName: new FormControl('', Validators.required),
+     phoneNumber: new FormControl('', [Validators.required, Validators.pattern('^(\([0-9]{3}\) |[0-9]{3}-)[0-9]{3}-[0-9]{4}$')]),
+     emailAddress: new FormControl('', [Validators.required, Validators.email]),
+     comment: new FormControl('', Validators.required),
+   });
+ }
+
 
 }
