146f09e46849a652cac495c6be6a2654705af4b2
aniketanand <anandaniket167@gmail.com>
2025-05-26 17:20:45 +0530

    t6 form added

--- src/app/templates/t6/t6-home/t6-home.component.ts
+++ src/app/templates/t6/t6-home/t6-home.component.ts
@@ -6,24 +6,40 @@ import { CustomerModel } from '../../../models/CustomerModel';
 import { StorageService } from '../../../services/storage.service';
 import { FeaturedPropertiesComponent } from '../../shared/featured-properties/featured-properties.component';
 import { SearchComponent } from '../../shared/search/search.component';
+import { CommonModule } from '@angular/common';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { FormBuilder, FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+// import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
 	selector: 'app-t6-home',
 	standalone: true,
-	imports: [RouterModule, SearchComponent, FeaturedPropertiesComponent],
+	imports: [RouterModule, SearchComponent, FeaturedPropertiesComponent,CommonModule, NgbModule, FormsModule, ReactiveFormsModule, MatDialogModule, MatFormFieldModule, MatInputModule],
 	templateUrl: './t6-home.component.html',
 	styleUrls: ['./t6-home.component.scss', '../t6.component.scss'],
 	providers: [Title, StorageService]
 })
 export class T6HomeComponent implements OnInit {
 	customer!: CustomerModel | null;
+  userForm!: FormGroup;
+
 
 	constructor(private router: Router,
+		private fb: FormBuilder,
 		private titleService: Title,
 	) { }
 
 	ngOnInit(): void {
 		this.titleService.setTitle("Home")
+		 this.userForm = this.fb.group({
+	firstName: new FormControl('', Validators.required),
+	phoneNumber: new FormControl('', [Validators.required, Validators.pattern('^(\([0-9]{3}\) |[0-9]{3}-)[0-9]{3}-[0-9]{4}$')]),
+	emailAddress: new FormControl('', [Validators.required, Validators.email]),
+	comment: new FormControl('', Validators.required),
+  });
 	}
 
 	searchProperties = (selectedFilters: any, searchByMap: boolean = false) => {
