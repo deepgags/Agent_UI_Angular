f9e857c610f5065a1abcf59dc79ee1c9abbf6ca1
i-am-milap <milap.shah@rocketmail.com>
2025-05-29 18:18:29 +0530

    signup ui fix

--- src/app/pages/public/register/register.component.ts
+++ src/app/pages/public/register/register.component.ts
@@ -1,43 +1,52 @@
-import { CommonModule } from '@angular/common';
-import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
-import { MatDialogModule } from '@angular/material/dialog';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material/input';
-import { MatSelectModule } from '@angular/material/select';
-import { Title } from '@angular/platform-browser';
-import { Router } from '@angular/router';
-import { AngularSvgIconModule } from 'angular-svg-icon';
-import { ImageCropperComponent } from 'ngx-smart-cropper';
-import { Pages } from '../../../enums/pages';
-import { BrokerageTypeModel } from '../../../models/BrokerageTypeModel';
-import { CustomerModel } from '../../../models/CustomerModel';
-import { BrokerageTypeService } from '../../../services/brokerage.service';
-import { CustomerService } from '../../../services/customer.service';
-import { LoadingService } from '../../../services/loading.service';
-import { NotificationService } from '../../../services/notification.service';
-
+import { CommonModule } from "@angular/common";
+import { Component, OnInit } from "@angular/core";
+import { FormBuilder, FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from "@angular/forms";
+import { MatDialogModule } from "@angular/material/dialog";
+import { Title } from "@angular/platform-browser";
+import { Router } from "@angular/router";
+import { AngularSvgIconModule } from "angular-svg-icon";
+import { ImageCropperComponent } from "ngx-smart-cropper";
+import { FloatLabelModule } from "primeng/floatlabel";
+import { InputMaskModule } from "primeng/inputmask";
+import { InputTextModule } from "primeng/inputtext";
+import { PasswordModule } from "primeng/password";
+import { SelectModule } from "primeng/select";
+import { Pages } from "../../../enums/pages";
+import { BrokerageTypeModel } from "../../../models/BrokerageTypeModel";
+import { CustomerModel } from "../../../models/CustomerModel";
+import { BrokerageTypeService } from "../../../services/brokerage.service";
+import { CustomerService } from "../../../services/customer.service";
+import { LoadingService } from "../../../services/loading.service";
+import { NotificationService } from "../../../services/notification.service";
 @Component({
-	selector: 'app-register',
-	imports: [CommonModule, AngularSvgIconModule, FormsModule, ReactiveFormsModule, MatDialogModule,
-		MatFormFieldModule, MatInputModule, MatSelectModule, ImageCropperComponent],
-	templateUrl: './register.component.html',
-	styleUrl: './register.component.scss'
+	selector: "app-register",
+	imports: [
+		CommonModule,
+		AngularSvgIconModule,
+		FormsModule,
+		ReactiveFormsModule,
+		MatDialogModule,
+		ImageCropperComponent,
+		InputMaskModule,
+		SelectModule,
+		FloatLabelModule,
+		InputTextModule,
+		PasswordModule,
+	],
+	templateUrl: "./register.component.html",
+	styleUrl: "./register.component.scss",
 })
-
 export class RegisterComponent implements OnInit {
-
 	customerForm!: FormGroup;
 
 	brokerageTypes: BrokerageTypeModel[] = [];
 
-	croppedProfileImage = '';
+	croppedProfileImage = "";
 	profileImageSource: any;
-	logoCroppedImage = '';
-	logoImageSource: string = '';
-	logoImagePath: string = '';
+	logoCroppedImage = "";
+	logoImageSource: string = "";
+	logoImagePath: string = "";
 	showSVG: boolean = false;
-
 
 	onFileChange(event: Event): void {
 		const input = event.target as HTMLInputElement;
@@ -48,8 +57,7 @@ export class RegisterComponent implements OnInit {
 			reader.onload = (e: any) => {
 				if (input?.id == "profileImageUpload") {
 					this.profileImageSource = e.target.result;
-				}
-				else {
+				} else {
 					this.logoImageSource = e.target.result;
 				}
 			};
@@ -74,25 +82,25 @@ export class RegisterComponent implements OnInit {
 		private titleService: Title,
 		private customerService: CustomerService
 	) {
-		this.titleService.setTitle("Register")
+		this.titleService.setTitle("Register");
 	}
 
 	ngOnInit() {
 		this.customerForm = this.fb.group({
-			businessName: new FormControl('', Validators.required),
+			businessName: new FormControl("", Validators.required),
 			brokerageType: new FormControl("", Validators.required),
-			firstName: new FormControl('', Validators.required),
-			lastName: new FormControl('', Validators.required),
-			phoneNumber: new FormControl('', [Validators.required, Validators.pattern('^(\([0-9]{3}\) |[0-9]{3}-)[0-9]{3}-[0-9]{4}$')]),
-			cellNumber: new FormControl('', [Validators.required, Validators.pattern('^(\([0-9]{3}\) |[0-9]{3}-)[0-9]{3}-[0-9]{4}$')]),
-			emailAddress: new FormControl('', [Validators.required, Validators.email]),
-			address: new FormControl(''),
-			password: new FormControl('', [Validators.required, Validators.pattern('^(?=.*[A-Z])(?=.*[0-9])(?=.*[a-z]).{8,}$')]),
-			confirmPassword: new FormControl('', Validators.required),
-			logoImage: new FormControl(''),
-			logoImagePath: new FormControl(''),
-			profileImage: new FormControl(''),
-			profileImagePath: new FormControl(''),
+			firstName: new FormControl("", Validators.required),
+			lastName: new FormControl("", Validators.required),
+			phoneNumber: new FormControl("", [Validators.required, Validators.pattern("^(([0-9]{3}) |[0-9]{3}-)[0-9]{3}-[0-9]{4}$")]),
+			cellNumber: new FormControl("", [Validators.required, Validators.pattern("^(([0-9]{3}) |[0-9]{3}-)[0-9]{3}-[0-9]{4}$")]),
+			emailAddress: new FormControl("", [Validators.required, Validators.email]),
+			address: new FormControl(""),
+			password: new FormControl("", [Validators.required, Validators.pattern("^(?=.*[A-Z])(?=.*[0-9])(?=.*[a-z]).{8,}$")]),
+			confirmPassword: new FormControl("", Validators.required),
+			logoImage: new FormControl(""),
+			logoImagePath: new FormControl(""),
+			profileImage: new FormControl(""),
+			profileImagePath: new FormControl(""),
 		});
 
 		this.getBrokerageTypes();
@@ -107,8 +115,10 @@ export class RegisterComponent implements OnInit {
 			error: () => {
 				this.notificationService.showNotification("Error occurred while getting brokerage types");
 			},
-			complete: () => { this.loadingService.loadingOff(); }
-		})
+			complete: () => {
+				this.loadingService.loadingOff();
+			},
+		});
 	}
 
 	brokerageChange(selectedBrokerageId: string): void {
@@ -118,7 +128,18 @@ export class RegisterComponent implements OnInit {
 
 	register() {
 		if (this.customerForm.valid) {
-			const { businessName, brokerageType, firstName, lastName, phoneNumber, cellNumber, emailAddress, address, password, confirmPassword } = this.customerForm.value;
+			const {
+				businessName,
+				brokerageType,
+				firstName,
+				lastName,
+				phoneNumber,
+				cellNumber,
+				emailAddress,
+				address,
+				password,
+				confirmPassword,
+			} = this.customerForm.value;
 			const params: CustomerModel = {
 				brokerageTypeId: brokerageType,
 				businessName: businessName,
@@ -133,7 +154,7 @@ export class RegisterComponent implements OnInit {
 				profileImage: this.croppedProfileImage,
 				logoImage: this.logoImageSource,
 				// logoImagePath: this.logoImagePath,
-			}
+			};
 
 			this.loadingService.loadingOn();
 			this.customerService.register(params).subscribe({
@@ -144,7 +165,7 @@ export class RegisterComponent implements OnInit {
 				error: (error: any) => {
 					this.loadingService.loadingOff();
 					this.notificationService.showNotification(error.error.message);
-				}
+				},
 			});
 		} else {
 			this.notificationService.showNotification("One or more required fields are missing or invalid.");
@@ -152,6 +173,6 @@ export class RegisterComponent implements OnInit {
 	}
 
 	redirectToLogin() {
-		this.router.navigateByUrl('login');
+		this.router.navigateByUrl("login");
 	}
 }
