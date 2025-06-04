8b7e5442db2320978737a4ec384345b8b2c11173
aniketanand <anandaniket167@gmail.com>
2025-06-04 17:30:39 +0530

    about us updated

--- src/app/templates/shared/about/about.component.ts
+++ src/app/templates/shared/about/about.component.ts
@@ -1,12 +1,28 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { SiteConfig } from '../../../models/SiteConfig';
+import { SiteConfigService } from '../../../services/site-config.service';
+import { PhoneSearch } from '../../../pipes/phoneSearch';
 
 @Component({
 	selector: 'app-about',
-	imports: [RouterModule],
+	imports: [RouterModule,PhoneSearch],
 	templateUrl: './about.component.html',
 	styleUrl: './about.component.scss'
 })
 export class AboutComponent {
+	siteConfig: SiteConfig | undefined;
+	siteConfigSubscription: any;
+	
 
+	constructor(private siteConfigService: SiteConfigService) {}
+
+	ngOnInit(): void {
+	
+		this.siteConfigSubscription = this.siteConfigService.currentConfig$.subscribe((config) => {
+			if (config) {
+				this.siteConfig = config;
+			}
+		});
+	}
 }
