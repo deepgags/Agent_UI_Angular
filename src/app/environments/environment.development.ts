e7e7b473a7f766d850f64f41c700f243a53a14cd
i-am-milap <milap.shah@rocketmail.com>
2025-08-13 11:51:46 +0530

    endpoint updated

--- src/app/environments/environment.development.ts
+++ src/app/environments/environment.development.ts
@@ -1,10 +1,10 @@
 export const environment = {
 	production: true,
-	agentApiUrl: "http://agentuiuxapi.sonnetinfotech.com:8082",
 	// agentApiUrl: "http://localhost:3000",
-	propertyApiUrl: "http://agentuiuxapi.sonnetinfotech.com:8081",
+	// propertyApiUrl: "http://localhost:8002/properties",
+	agentApiUrl: "http://agentuiuxapi.sonnetinfotech.com:8082",
+	propertyApiUrl: "http://agentuiuxapi.sonnetinfotech.com:8081/properties",
 	imageUrl: "http://images.sonnetinfotech.com/",
 	templateUrl: "/templates",
-	propertySearchUrl: "/properties",
 	interestedUserUrl: "/interestdUsers",
 };
--- src/app/environments/environment.ts
+++ src/app/environments/environment.ts
@@ -1,8 +1,7 @@
 export const environment = {
 	production: true,
 	agentApiUrl: "http://agentuiuxapi.sonnetinfotech.com:8082",
-	propertyApiUrl: "http://agentuiuxapi.sonnetinfotech.com:8081",
+	propertyApiUrl: "http://agentuiuxapi.sonnetinfotech.com:8081/properties",
 	imageUrl: "http://images.sonnetinfotech.com/",
-	propertySearchUrl: "/properties",
 	interestedUserUrl: "/interestdUsers",
 };
