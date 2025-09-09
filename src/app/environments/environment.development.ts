10a3337d75bb9ea0c27566dae06dce5686e2e2be
aniketanand <anandaniket167@gmail.com>
2025-09-09 17:18:18 +0530

    UI

--- src/app/environments/environment.development.ts
+++ src/app/environments/environment.development.ts
@@ -1,7 +1,7 @@
 export const environment = {
 	production: true,
-	baseUrl: "http://localhost:3000",
-	// agentApiUrl: "http://agentuiuxapi.sonnetinfotech.com:8082",
+	// baseUrl: "http://localhost:3000",
+	baseUrl: "http://agentuiuxapi.sonnetinfotech.com:8082",
 	imageUrl: "http://images.sonnetinfotech.com/",
 	templateUrl: "/templates",
 	interestedUserUrl: "/interestdUsers",
