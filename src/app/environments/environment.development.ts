b2940fd134951aa9b2f2424b1d1ef199bbf5bb46
deepgags <deepgags@gmail.com>
2025-10-08 15:20:02 +0000

    committing

--- src/app/environments/environment.development.ts
+++ src/app/environments/environment.development.ts
@@ -1,6 +1,6 @@
 export const environment = {
 	production: true,
-	baseUrl: "http://localhost:3000",
-	// baseUrl: "http://agentuiuxapi.sonnetinfotech.com:8082",
+	//baseUrl: "http://localhost:3000",
+	  baseUrl: "http://agentuiuxapi.sonnetinfotech.com:8082",
 	imageUrl: "http://images.sonnetinfotech.com/",
 };
