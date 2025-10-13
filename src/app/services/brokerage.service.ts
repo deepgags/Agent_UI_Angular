9023502b901fa14aa6569aaad14c3d9f0d6a3bf7
Milap <milap.codes@gmail.com>
2025-10-13 15:58:55 +0530

    autocomplete for brokerages, brokerage logo preview, improved featured listing

--- src/app/services/brokerage.service.ts
+++ src/app/services/brokerage.service.ts
@@ -1,19 +1,15 @@
 import { HttpClient } from "@angular/common/http";
-import { inject, Injectable, resource, signal } from "@angular/core";
-import { catchError, map, Observable, throwError } from "rxjs";
+import { Injectable } from "@angular/core";
 import { environment } from "../environments/environment.development";
-import { BrokerageTypeModel } from "../models/BrokerageTypeModel";
 
 @Injectable({
 	providedIn: "root",
 })
 export class BrokerageTypeService {
-	private brokerageTypes: BrokerageTypeModel[] = [];
-	query = signal<string>("");
 	private baseUrl: string = environment.baseUrl;
 
 	constructor(private http: HttpClient) {}
-	getBrokerageTypes(): Observable<BrokerageTypeModel[]> {
-		return this.http.get<BrokerageTypeModel[]>(`${this.baseUrl}/brokerages`);
+	getBrokerageTypes() {
+		return this.http.get(`${this.baseUrl}/properties/brokerages`);
 	}
 }
