5e4605c48e8d94b8386020abd81e998bac3410b6
Milap <milap.codes@gmail.com>
2025-11-24 11:58:46 +0530

    Side menu issue fixed

--- src/app/components/menu/menu.component.ts
+++ src/app/components/menu/menu.component.ts
@@ -3,10 +3,11 @@ import { Component } from "@angular/core";
 import { RouterModule } from "@angular/router";
 import { MenuItem } from "../../models/MenuItem";
 import { SharedDataService } from "../../services/shared-data.service";
+import { SideMenuComponent } from "../side-menu/side-menu.component";
 
 @Component({
 	selector: "app-menu",
-	imports: [CommonModule, RouterModule],
+	imports: [CommonModule, RouterModule, SideMenuComponent],
 	templateUrl: "./menu.component.html",
 	styleUrl: "./menu.component.scss",
 })
