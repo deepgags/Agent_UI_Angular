b998f056326842ebf870701d25da0e76d8e6a068
aniketanand <anandaniket167@gmail.com>
2025-12-17 12:49:28 +0530

    ui changes

--- src/app/templates/t5/t5-header/t5-header.component.ts
+++ src/app/templates/t5/t5-header/t5-header.component.ts
@@ -4,16 +4,17 @@ import { AngularSvgIconModule } from "angular-svg-icon";
 import { MenuComponent } from "../../../components/menu/menu.component";
 import { SiteConfig } from "../../../models/SiteConfig";
 import { PhoneNumberFormatPipe } from "../../../pipes/phone-format";
+import { environment } from "../../../environments/environment.development";
 
 @Component({
 	selector: "app-t5-header",
-	imports: [AngularSvgIconModule, PhoneNumberFormatPipe, RouterModule, MenuComponent, ],
+	imports: [AngularSvgIconModule, PhoneNumberFormatPipe, RouterModule, MenuComponent],
 	templateUrl: "./t5-header.component.html",
 	styleUrls: ["./t5-header.component.scss", "../t5.component.scss"],
 })
 export class T5HeaderComponent implements OnInit {
 	@Input("siteConfig") siteConfig: SiteConfig | any;
-
+	localImageUrl = environment.localImageUrl;
 	constructor() {}
 
 	ngOnInit(): void {}
