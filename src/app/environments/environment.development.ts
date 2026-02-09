da37d7fdd98d0801ff4fb9c5a55e43ca06599558
ms <ms@mss-MacBook-Pro.local>
2026-02-09 18:03:55 +0530

    environment organize

--- src/app/environments/environment.development.ts
+++ src/app/environments/environment.development.ts
@@ -1,7 +1,10 @@
 export const environment = {
 	production: true,
-	baseUrl: "http://localhost:3000",
-	// baseUrl: "http://agentuiuxapi.sonnetinfotech.com:8082",
-	propertyImageUrl: "http://images.sonnetinfotech.com/",
+	// baseUrl: "http://localhost:3000",
+	// localImageUrl: "http://localhost:3000",
+
+	baseUrl: "http://agentuiuxapi.sonnetinfotech.com:8082",
 	localImageUrl: "http://agentuiuxapi.sonnetinfotech.com:8082",
+
+	propertyImageUrl: "http://images.sonnetinfotech.com/",
 };
--- src/app/environments/environment.ts
+++ src/app/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
 	production: true,
 	baseUrl: "http://agentuiuxapi.sonnetinfotech.com:8082",
-	propertyImageUrl: "http://images.sonnetinfotech.com/",
 	localImageUrl: "http://agentuiuxapi.sonnetinfotech.com:8082",
+	propertyImageUrl: "http://images.sonnetinfotech.com/",
 };
